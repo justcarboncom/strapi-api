46ad40dd840503fe28e356a2ff9a71f9f88d85ef
Jordan <jordan.imran@predixa.ai>
2022-04-28 22:38:12 +0400

    added s3

--- config/database.js
+++ config/database.js
@@ -4,11 +4,11 @@ module.exports = ({ env }) => {
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'jc-strapi-dev.c19d2aywvrnd.us-east-1.rds.amazonaws.com'),
+      host: env('DATABASE_HOST', 'localhost'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', process.env.DATABASE_PASSWORD),
+      database: env('DATABASE_NAME', 'justcarbon-api-strapi'),
+      user: env('DATABASE_USERNAME', 'strapi'),
+      password: env('DATABASE_PASSWORD', 'strapi'),
       ssl: env.bool('DATABASE_SSL', false),      
     },
   },
--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,8 @@
-module.exports = [
+module.exports = ({env}) => {
+  const bucket = env('AWS_BUCKET_NAME');
+  const s3 = `https://${bucket}.s3.amazonaws.com`;
+  return [
   'strapi::errors',
-  'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
@@ -9,4 +11,31 @@ module.exports = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-];
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            "'unsafe-inline'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            s3,
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            s3,
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+]};
