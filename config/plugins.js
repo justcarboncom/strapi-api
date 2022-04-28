12fd7f8bca00d85cc5124f9ddf7f1c1b5cff221a
Jordan <jordan.imran@predixa.ai>
2022-04-28 22:38:34 +0400

    added s3

--- config/plugins.js
+++ config/plugins.js
@@ -5,9 +5,9 @@ module.exports = ({ env }) => ({
             providerOptions: {
                 accessKeyId: env('AWS_ACCESS_KEY_ID'),
                 secretAccessKey: env('AWS_ACCESS_SECRET'),
-                region: env('AWS_REGION'),
-                params: {
-                    Bucket: env('AWS_BUCKET_NAME'),
+                    region: env('AWS_REGION'),
+                    params: {
+                        Bucket: env('AWS_BUCKET_NAME'),
                 },
             },
         },
