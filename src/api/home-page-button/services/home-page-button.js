fc97b1458d4aa1bde6b726a45c35c491fc89bba0
DMITRI <1124004@gmail.com>
2022-05-06 13:34:01 +0300

    [Strapi] added home-page-buttons

---
+++ src/api/home-page-button/services/home-page-button.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * home-page-button service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::home-page-button.home-page-button');
