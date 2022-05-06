b66894e1d7270c89e3fc1404f8e1d78c57de7531
DMITRI <1124004@gmail.com>
2022-05-06 17:49:02 +0300

    [Strapi] added simple-and-easy-offset

---
+++ src/api/simple-and-easy-offset/controllers/simple-and-easy-offset.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  simple-and-easy-offset controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::simple-and-easy-offset.simple-and-easy-offset');
