7e879181ae66ac75b0ba472f1a42acacfc3cca0b
DMITRI <1124004@gmail.com>
2022-05-10 17:09:39 +0300

    NavBar and OffsetPage

---
+++ src/api/offset-page/controllers/offset-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  offset-page controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::offset-page.offset-page');
