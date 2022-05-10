f169c578cd3ff8a0b437c9c3ca810ea4987f6258
DMITRI <1124004@gmail.com>
2022-05-10 15:13:42 +0300

    News-room

--- src/api/test-type/controllers/test-type.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  test-type controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::test-type.test-type');
