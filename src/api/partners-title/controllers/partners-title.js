c59044f0ffff25feed95658f84e23724595ffa5b
DMITRI <1124004@gmail.com>
2022-05-26 11:53:32 +0300

    added partners and partnersTitle types

---
+++ src/api/partners-title/controllers/partners-title.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  partners-title controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::partners-title.partners-title');
---
+++ src/api/partners-title/routes/partners-title.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * partners-title router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::partners-title.partners-title');
