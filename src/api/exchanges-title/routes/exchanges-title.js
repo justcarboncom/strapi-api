5b7bab655e2051bbdc7432a3cb1a674c7b3d8dd2
DMITRI <1124004@gmail.com>
2022-06-06 11:07:39 +0300

    Blog, NavBar

---
+++ src/api/exchanges-title/routes/exchanges-title.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * exchanges-title router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::exchanges-title.exchanges-title');
