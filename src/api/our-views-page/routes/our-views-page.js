71c8e781cef489d5765d88ef3902eae34de9092c
DMITRI <1124004@gmail.com>
2022-06-13 17:06:09 +0300

    OurViews type

---
+++ src/api/our-views-page/routes/our-views-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * our-views-page router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::our-views-page.our-views-page');
