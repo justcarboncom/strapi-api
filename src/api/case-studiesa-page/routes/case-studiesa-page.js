e719c46d632469217845b6f4c139443f7720cfa2
DMITRI <1124004@gmail.com>
2022-07-08 14:19:41 +0300

    [CaseStudiesPage] added fields

---
+++ src/api/case-studiesa-page/routes/case-studiesa-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * case-studiesa-page router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::case-studiesa-page.case-studiesa-page');
