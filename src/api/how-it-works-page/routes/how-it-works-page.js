45007fa871a1810e8c4a3677d08b5a442d24bdf7
DMITRI <1124004@gmail.com>
2022-08-01 14:23:25 +0300

    HowItWorksPage type

---
+++ src/api/how-it-works-page/routes/how-it-works-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * how-it-works-page router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::how-it-works-page.how-it-works-page');
