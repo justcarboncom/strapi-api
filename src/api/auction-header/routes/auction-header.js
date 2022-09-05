4c3045e22c8dfe33a34402a4800ce24a5e4a67a2
DMITRI <1124004@gmail.com>
2022-09-05 21:14:13 +0300

    remove Auction Header & Announcement

--- src/api/auction-header/routes/auction-header.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * auction-header router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::auction-header.auction-header');
